aba8e82455fe8a6e87313b606c71f10fd60ac0fe
JeffChoi <ihello0720@gmail.com>
2018-07-01 22:45:36 +0900

    Repository  정의

---
+++ src/main/java/jeff/com/polls/repository/UserRepository.java
@@ -0,0 +1,24 @@
+package jeff.com.polls.repository;
+
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import jeff.com.polls.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsernameOrEmail(String username, String email);
+
+    List<User> findByIdIn(List<Long> userIds);
+
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+}
